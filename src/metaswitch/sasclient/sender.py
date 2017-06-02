96a0a4738e0e87f0cb0cea24363f666d7266b0f2
huws <huw.smithson@metaswitch.com>
2017-06-02 13:19:13 +0000

    Correct exception type

--- src/metaswitch/sasclient/sender.py
+++ src/metaswitch/sasclient/sender.py
@@ -125,10 +125,10 @@ class MessageSender(threading.Thread):
         try:
             self._sas_sock.shutdown(socket.SHUT_RDWR)
             self._sas_sock.close()
-        except OSError:
+        except Exception:
             # Ignore errors that occur while trying to close a socket.  If the
             # connection has gone away, we don't have anything more to do.
-            logger.info("Hit error closing socket - ignore")
+            logger.debug("Hit error closing socket - ignore")
 
     def send_message(self, message):
         """
